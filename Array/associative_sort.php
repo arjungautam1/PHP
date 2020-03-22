90aa260120f371bc1a36302d846b4f5eb5cc6045
Laserarjun <laserarjun876@gmail.com>
2020-03-22 21:39:03 +0545

    Sorting

--- Array/associative_sort.php
+++ Array/associative_sort.php
@@ -6,13 +6,21 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Sorting an associative sort </h1>
+    <h1>Sorting by value and keys  </h1>
     <?php
+    echo "Sorting by value :<br>";
     $name=array("Hari"=>12,"Ram"=>10,"Pooja"=>23,"Arjun"=>11);
-    asort($name);
+    asort($name);//ascending
     print_r($name);
     echo "<br>";
-    arsort($name);
+    arsort($name);//descending
+    print_r($name);
+    echo "<br>";
+    echo "Sorting by key :<br>";
+    ksort($name);//ascending 
+    print_r($name);
+    krsort($name);//decending
+    echo "<br>";
     print_r($name);
 
     ?>
