374f3770a4df2f008a90770cddfc6b08e70a0755
Laserarjun <laserarjun876@gmail.com>
2020-03-22 22:28:52 +0545

    function

---
+++ Function/func.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Using function in php</h1>
+    <?php
+    function add($num1,$num2)
+    {
+        $sum=$num1+$num2;
+        echo "Sum of the numbers is :".$sum;
+    }
+    add(10,20);
+    ?>
+</body>
+</html>
